08e2ce39a203229f50188a4d9b0061ca6144bdd8
mrari007 <marti21248@gmail.com>
2021-02-05 17:28:50 +0200

    step 11 done

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -9,17 +9,27 @@ import java.util.*;
 
 public class TrainingRecord {
     private List<Entry> tr;
-    
     public TrainingRecord() {
         tr = new ArrayList<Entry>();
     } //constructor
     
     // add a record to the list
    public void addEntry(Entry e){
+	  // if(e.getNameDayMoYe() == lookupEntryName (e.getName(), e.getDay(), e.getMonth(), e.getYear()))
+	   for (int i = 0; i<tr.size(); i++)
+	   {
+		    Entry count = tr.get(i);
+		    if(count.getName().equalsIgnoreCase(e.getName()) && count.getDay() == e.getDay() && count.getMonth() == e.getMonth() && count.getYear() == e.getYear())
+		    {
+		    	System.out.println("Record already added!");
+		    	return;
+		    }
+	   }
        tr.add(e);    
    } // addClass
    
    // look up the entry of a given day and month
+   
    public String lookupEntry (int d, int m, int y) {
        ListIterator<Entry> iter = tr.listIterator();
        String result = "No entries found";
@@ -30,7 +40,6 @@ public class TrainingRecord {
             }
        return result;
    } // lookupEntry
-   		
    public String lookAllEntries (int d, int m, int y)
    {
 	    ListIterator<Entry> iter = tr.listIterator();
@@ -39,6 +48,29 @@ public class TrainingRecord {
 	       while (iter.hasNext()) {
 	          Entry current = iter.next();
 	          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+	          {
+	        	  if(count ==0)
+	        	  {
+			             result = current.getEntry();
+	        	  }
+	        	  else
+	        	  {
+	        		  result = result +  current.getEntry();
+	        	  }
+	        	  count++;
+	          }
+	            }
+	       return result;
+   }
+   
+   public String lookAllByName (String n)
+   {
+	    ListIterator<Entry> iter = tr.listIterator();
+	       String result = "No entries found";
+	       int count = 0;
+	       while (iter.hasNext()) {
+	          Entry current = iter.next();
+	          if (n.equalsIgnoreCase(current.getName()))
 	          {
 	        	  if(count ==0)
 	        	  {
